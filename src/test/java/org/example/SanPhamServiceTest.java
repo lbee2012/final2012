74583af74a68f777d6b016e73c29642abaa33619
Long Hoang (Eric) <hoangflong2012@gmail.com>
2025-10-29 14:45:52 +0700

    ok

--- src/test/java/org/example/SanPhamServiceTest.java
+++ src/test/java/org/example/SanPhamServiceTest.java
@@ -19,34 +19,52 @@ class SanPhamServiceTest {
         boolean result = spService.add(sp);
         assertTrue(result);
     }
+    @Test
+    void add_soLuong_throw() {
+        SanPham sp = new SanPham("SP001", "Dien thoai", 12, 5000f, 100000, "Dien tu");
+        assertThrows(IllegalArgumentException.class, () -> spService.add(sp));
+    }
 
-    @Test
-    void add_soLuong_lessThanMin_throw() {
-        SanPham sp = new SanPham("SP001", "Dien thoai", 12, 5000f, 0, "Dien tu");
-        assertThrows(IllegalArgumentException.class, () -> spService.add(sp));
-    }
-    @Test
-    void add_soLuong_moreThanMax_throw() {
-        SanPham sp = new SanPham("SP001", "Dien thoai", 12, 5000f, 101, "Dien tu");
-        assertThrows(IllegalArgumentException.class, () -> spService.add(sp));
-    }
-    @Test
-    void add_soLuong_max_ok() {
-        SanPham sp = new SanPham("SP001", "Dien thoai", 12, 5000f, 100, "Dien tu");
-        boolean result = spService.add(sp);
-        assertTrue(result);
-    }
-    @Test
-    void add_soLuong_min_ok() {
-        SanPham sp = new SanPham("SP001", "Dien thoai", 12, 5000f, 1, "Dien tu");
-        boolean result = spService.add(sp);
-        assertTrue(result);
-    }
     @Test
     void add_maSP_duplicated_throw() {
         SanPham sp = new SanPham("SP001", "Dien thoai", 12, 5000f, 10, "Dien tu");
         SanPham sp1 = new SanPham("SP001", "Dien thoai", 12, 5000f, 10, "Dien tu");
         spService.add(sp);
         assertThrows(IllegalArgumentException.class, () -> spService.add(sp1));
+    }
+
+    @Test
+    void add_soLuong_upperBoundary_ok() {
+        SanPham sp = new SanPham("SP001", "Dien thoai", 12, 5000f, 100, "Dien tu");
+        boolean result = spService.add(sp);
+        assertTrue(result);
+    }
+    @Test
+    void add_soLuong_belowBoundary_ok() {
+        SanPham sp = new SanPham("SP001", "Dien thoai", 12, 5000f, 1, "Dien tu");
+        boolean result = spService.add(sp);
+        assertTrue(result);
+    }
+    @Test
+    void add_soLuong_moreThan_upperBoundary_throw() {
+        SanPham sp = new SanPham("SP001", "Dien thoai", 12, 5000f, 101, "Dien tu");
+        assertThrows(IllegalArgumentException.class, () -> spService.add(sp));
+    }
+    @Test
+    void add_soLuong_lessThan_upperBoundary_ok() {
+        SanPham sp = new SanPham("SP001", "Dien thoai", 12, 5000f, 99, "Dien tu");
+        boolean result = spService.add(sp);
+        assertTrue(result);
+    }
+    @Test
+    void add_soLuong_moreThan_belowBoundary_ok() {
+        SanPham sp = new SanPham("SP001", "Dien thoai", 12, 5000f, 2, "Dien tu");
+        boolean result = spService.add(sp);
+        assertTrue(result);
+    }
+    @Test
+    void add_soLuong_lessThan_belowBoundary_throw() {
+        SanPham sp = new SanPham("SP001", "Dien thoai", 12, 5000f, 0, "Dien tu");
+        assertThrows(IllegalArgumentException.class, () -> spService.add(sp));
     }
 }
--- src/test/java/org/example/SumTest.java
+++ src/test/java/org/example/SumTest.java
@@ -13,33 +13,33 @@ class SumTest {
     }
 
     @Test
-    void sum_odd_ok() {
+    void sum_odd_result_ok() {
         int result = s.sumOdd();
         int expected = 250000;
         assertEquals(expected, result);
     }
     @Test
-    void sum_odd_throw() {
+    void sum_odd_zero_throw() {
         int result = s.sumOdd();
         int unexpected = 0;
         assertNotEquals(unexpected, result);
     }
     @Test
-    void sum_odd_249999() {
+    void sum_odd_negative_throw() {
+        int result = s.sumOdd();
+        int unexpected = -250000;
+        assertNotEquals(unexpected, result);
+    }
+    @Test
+    void sum_odd_lessThan_result_249999() {
         int result = s.sumOdd();
         int expected = 249999;
         assertNotEquals(expected, result);
     }
     @Test
-    void sum_odd_250001() {
+    void sum_odd_moreThan_result_250001() {
         int result = s.sumOdd();
         int expected = 250001;
         assertNotEquals(expected, result);
-    }
-    @Test
-    void sum_odd_negative() {
-        int result = s.sumOdd();
-        int unexpected = -250000;
-        assertNotEquals(unexpected, result);
     }
 }
